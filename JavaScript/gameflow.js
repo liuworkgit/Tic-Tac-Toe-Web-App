d006c984e4cd96742c606a7d4941a04f0716528f
moses21 <moses21@student.ubc.ca>
2024-05-26 11:35:08 -0700

    finished documentation and function headers of gameflow

--- JavaScript/gameflow.js
+++ JavaScript/gameflow.js
@@ -8,7 +8,76 @@ import { createPlayer } from "./player";
  * 
  * Manages the logic and gameplay of the tic-tac-toe game.
  * 
+ * @function newGame - starts a new game by getting the players' names 
+ * and randomising who goes first
+ * @function restartGame - resets the gameboard and reselects a 
+ * random player to start
+ * @function makeMove - lets the current player make a move.
+ * @function endGame - ends the game by announcing who won/tied and 
+ * asking if the players want to go again.
  */
 function createGameflow () {
+    let board = gameboard;
+    let playerOne;
+    let playerTwo;
+    let currentPlayer;
 
+    /**
+     * starts a new game by getting the players' names and 
+     * randomising who goes first
+     * @return void
+     */
+    const newGame = function () {}
+
+    /**
+     * randomly picks a player to go first
+     * @return void
+     */
+    const randomizeWhoStarts = function () {}
+
+    /**
+     * resets the gameboard and reselects a random player to
+     * start
+     * @return void
+     */
+    const restartGame = function () {}
+
+    /**
+     * lets the current player make a move.
+     * @return void
+     */
+    const makeMove = function () {}
+
+    /**
+     * returns true if there is a tie.
+     * @return boolean
+     */
+    const isTie = function () {}
+
+    /**
+     * returns true if there is a win (i.e. a row/col filled
+     * by a player)
+     * @return boolean
+     */
+    const isWin = function () {}
+
+    /**
+     * changes whose turn it currently is.
+     * @return void
+     */
+    const changeTurn = function () {}
+
+    /**
+     * ends the game by announcing who won/tied and asking 
+     * if the players want to go again.
+     * @return void
+     */
+    const endGame = function () {}
+
+    /**
+     * increases the score of the winner.
+     * @param {player} winner - the winning player
+     * @return void
+     */
+    const changeWinnerScore = function (winner) {}
 }
