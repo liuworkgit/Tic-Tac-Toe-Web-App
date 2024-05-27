b65d8daa05fe7a82a8f09a0ddcee900a082a6941
moses21 <moses21@student.ubc.ca>
2024-05-27 11:07:54 -0700

    added getters to player module

--- JavaScript/player.js
+++ JavaScript/player.js
@@ -6,20 +6,51 @@
  * Represents a player of the tic-tac-toe game.
  * 
  * @param {string} name - the name of the player
- * @field numWins - the number of times the player won
- * @function increaseNumWins - increase numWins by 1
+ * @param {number} id - the player's id number
+ * @function increaseNumWins - increase the number of wins by 1
+ * @function getNumWins - returns the number of wins
+ * @function getName - returns the player's name
+ * @function getId - returns the player's id
  */
-function createPlayer(name) {
+function createPlayer(name, id) {
+    /**
+     * @field numWins - the number of times the player won
+     */
     let numWins = 0;
 
     /**
-     * Increases the number of wins by 1
+     * Increases the number of wins by 1.
+     * @returns void
      */
     const increaseNumWins = function () {
         numWins++;
     };
 
-    return { name, numWins, increaseNumWins };
+    /**
+     * returns the number of wins.
+     * @returns number
+     */
+    const getNumWins = function () {
+        return numWins;
+    }
+
+    /**
+     * returns the player's name.
+     * @returns string
+     */
+    const getName = function () {
+        return name;
+    }
+
+    /**
+     * returns the player's id.
+     * @returns number
+     */
+    const getId = function () {
+        return id;
+    }
+
+    return { increaseNumWins, getNumWins, getName, getId };
 };
 
 export { createPlayer };
